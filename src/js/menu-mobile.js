83a068d11aaf3a565d0d1341bd8a2492efc2e385
Sarinha090115 <sarasoaresbarbosa09@gmail.com>
2024-11-13 09:33:38 -0300

    V0.1

---
+++ src/js/menu-mobile.js
@@ -0,0 +1,11 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventdefault();
+    const nav = document.getElementById('navegacao')
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
